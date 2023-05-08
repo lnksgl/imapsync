e6cb88410793e764f0dbf294130efc0ab1406031
lnksgl <alexshlyck@gmail.com>
2023-05-08 19:30:19 +0300

    add web

---
+++ startimapsync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#переход в каталог откуда был запущен данный скрипт
+cd `dirname $0`
+#запуск imapsync с нужными параметрами
+imapsync/./imapsync --host1 $1 --user1 $2 --password1 $3 --ssl1 --host2 $4 --user2 $5 --password2 $6 --ssl2	   
