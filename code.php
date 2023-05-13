2701fb76f22ef79ee11f2d67235d1c4dc8e482dc
lnksgl <alexshlyck@gmail.com>
2023-05-14 02:32:05 +0300

    change cron configuration and script generation

--- code.php
+++ code.php
@@ -8,20 +8,18 @@
         $msrv2 = $_POST['msrv2'];
         
         // save sh script
-        $file = fopen("cron/migration-".$mail2."-".date('Y-m-d H:i:s').".sh", "w");
+        $name = "migration-".$mail2."-".date('Y-m-d-H-i-s').".sh";
+        $file = fopen("cron/".$name, "w");
         
         fwrite($file, "#!/bin/bash\n");
+        fwrite($file, "mv ~/Documents/imapsync/cron/".$name." ~/Documents/imapsync/cron/history/".$name."\n");
         fwrite($file, "cd `dirname $0`\n");
-        fwrite($file, "cron/./imapsync --host1 ".$msrv1
+        fwrite($file, "~/Documents/imapsync/cron/imapsync/./imapsync --host1 ".$msrv1
         	." --user1 ".$mail1
         	." --password1 ".$pass1
         	." --ssl1 --host2 ".$msrv2
         	." --user2 ".$mail2
-        	." --password2 ".$pass2." --ssl2");
-        	
-        fwrite($file, "cd `dirname $0`\n");	
-        fwrite($file, "name=$0\n");
-        fwrite($file, "mv ~/Documents/imapsync/cron/${name} ~/Documents/imapsync/cron/history/${name}`\n");	
+        	." --password2 ".$pass2." --ssl2\n");	
         
 	fclose($file);
     }
--- cron/migration-ytre-2023-05-09 18:39:03.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-cron/./imapsync --host1 ytryer --user1 123 --password1 ytter --ssl1 --host2 ytryer --user2 ytre --password2 yteer --ssl2cd `dirname $0`
-name=$0
-mv ~/Documents/imapsync/cron/ ~/Documents/imapsync/cron/history/`
